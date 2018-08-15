70b4b72109394cb457b39c1e277571c2f97430b3
Kristine <kristinetrona@gmail.com>
2018-08-15 11:54:51 +0200

    getCosts function added

--- crops.js
+++ crops.js
@@ -5,12 +5,16 @@ class Crop {
     getYieldInEuros(){
         return (this.getYieldInKg() * this.price)
     }
+    getCosts(){
+        return this.costs
+    }
   }
 
 class Wheat extends Crop {
     constructor(acres){
         super(acres)
     this.price = 1.5
+    this.costs = 340*acres
     }
     getYieldInKg() {
       return Math.pow(this.acres * 1.5, 1.3)
@@ -21,6 +25,7 @@ class Wheat extends Crop {
     constructor(acres){
         super(acres)
     this.price = 2
+    this.costs = 166*acres
     }
 
     getYieldInKg() {
--- index.spec.js
+++ index.spec.js
@@ -23,6 +23,7 @@ test('The Wheat class calculates the proper yield', () => {
   expect(wheat.getYieldInKg()).toBeCloseTo(674.4, 2)
 })
 
+
 test('The Sugarcane class can calculate a yield', () => {
     const sugarcane = new Sugarcane(100)
     expect(sugarcane.getYieldInKg()).toBeDefined()
@@ -33,10 +34,12 @@ test('The Sugarcane class can calculate a yield', () => {
     expect(sugarcane.getYieldInKg()).toBeCloseTo(453.39, 2)
   })
 
+
   test('A crop can store its area size', () => {
     const crop = new Crop(12345)
     expect(crop.acres).toBe(12345)
   })
+
 
   test('Wheat class calculates the proper yield value', () => {
     const wheat = new Wheat(100)
@@ -51,4 +54,20 @@ test('The Sugarcane class can calculate a yield', () => {
   test('Sugarcane class calculates the proper yield value', () => {
     const sugarcane = new Sugarcane(0)
     expect(sugarcane.getYieldInEuros()).toBe(0)
+  })
+
+
+  test('Sugarcane class calculates the proper costs value', () => {
+    const sugarcane = new Sugarcane(0)
+    expect(sugarcane.getCosts()).toBe(0)
+  })
+  
+  test('Sugarcane class calculates the proper costs value', () => {
+    const sugarcane = new Sugarcane(456)
+    expect(sugarcane.getCosts()).toBe(75696)
+  })
+  
+  test('Wheat class calculates the proper costs value', () => {
+    const wheat = new Wheat(123)
+    expect(wheat.getCosts()).toBe(41820)
   })
